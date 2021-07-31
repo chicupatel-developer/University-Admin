28e14d957b20458cf1b652c3ac609dcaeec0cfd0
Ankithorizon <ankitjpatel2007@hotmail.com>
2021-07-30 20:50:34 -0500

    doc-wip1.4

--- ngApp/e2e/src/app.e2e-spec.ts
+++ ngApp/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('fcuk app is running!');
+    expect(await page.getTitleText()).toEqual('ngApp app is running!');
   });
 
   afterEach(async () => {
